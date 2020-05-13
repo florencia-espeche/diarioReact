f3e24737c156ef63ca78bfe86af37cff9519ffba
Florencia Espeche <maria.espeche@globant.com>
2020-05-13 14:35:58 -0300

    add layout styles

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import './Layout.css';
 
-const Layout = (props) => {
+const Layout = ({children}) => {
 
     return (
-        <>
+        <div className="layout-container">
             <Header />
-                {props.children}
+                {children}
             <Footer />
-        </>
+        </div>
     )
 }
 
